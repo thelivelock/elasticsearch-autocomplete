92c09f86f8da491e1e89f5f7faa83ef0cf238f01
Milos Biljanovic <milosbiljanovic@gmail.com>
2023-02-16 12:25:59 +0100

    Upgraded to SpringBoot 3.0

--- src/main/java/com/thelivelock/elasticsearch_autocomplete/repository/UserRepository.java
+++ src/main/java/com/thelivelock/elasticsearch_autocomplete/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.thelivelock.elasticsearch_autocomplete.repository;
 
 import com.thelivelock.elasticsearch_autocomplete.model.User;
-import org.elasticsearch.index.query.QueryBuilder;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +9,4 @@ import java.util.List;
 @Repository
 public interface UserRepository extends ElasticsearchRepository<User, Long> {
     List<User> findAll();
-
-    List<User> search(QueryBuilder query);
 }
--- src/main/java/com/thelivelock/elasticsearch_autocomplete/service/UserService.java
+++ src/main/java/com/thelivelock/elasticsearch_autocomplete/service/UserService.java
@@ -1,19 +1,26 @@
 package com.thelivelock.elasticsearch_autocomplete.service;
 
+import co.elastic.clients.elasticsearch._types.query_dsl.Operator;
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
 import com.thelivelock.elasticsearch_autocomplete.model.User;
 import com.thelivelock.elasticsearch_autocomplete.repository.UserRepository;
-import org.elasticsearch.index.query.MatchQueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.client.elc.NativeQuery;
+import org.springframework.data.elasticsearch.client.elc.QueryBuilders;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
-
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private ElasticsearchOperations elasticsearchOperations;
 
     public List<User> listAll() {
         return this.userRepository.findAll();
@@ -28,7 +35,11 @@ public class UserService {
     }
 
     public List<User> search(String keywords) {
-        MatchQueryBuilder searchByCountries = QueryBuilders.matchQuery("country", keywords);
-        return this.userRepository.search(searchByCountries);
+        Float nonExistingBoost = null; // even though it exists in SpringBoot, ElasticSearch has no boost for this type of query
+        // when you analyze what matchQuery returns, it also has nothing related to boost
+        Query query = QueryBuilders.matchQuery("country", keywords, Operator.Or, nonExistingBoost)._toQuery();
+        NativeQuery nativeQuery = NativeQuery.builder().withQuery(query).build();
+        SearchHits<User> result = this.elasticsearchOperations.search(nativeQuery, User.class);
+        return result.stream().map(SearchHit::getContent).collect(Collectors.toList());
     }
 }
